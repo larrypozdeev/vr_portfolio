c842fcaa5501bf504863286fafbac41d032641ae
Larry Pozdeev <larrypozdeev@gmail.com>
2024-03-13 01:44:10 -0700

    adds boundary for max distance from center

--- src/App.tsx
+++ src/App.tsx
@@ -49,39 +49,48 @@ function App() {
     return (
         <>
             <VRButton />
-            <Canvas>
-                <drei.Stars radius={500} depth={100} count={1000} factor={5} />
-                <fog attach="fog" args={['#202025', 0, 80]} />
-                <Frame position={[10, 0, 0.5]} />
-                <FrameVideo url="/10.mp4" position={[-3, 0, 1]} rotation={[0, 0.9, 0]} />
-                <drei.Text3D
-                    position={[-2, 1, -1.5]}
-                    font="/Inter_Bold.json"
-                    curveSegments={32}
-                    bevelEnabled
-                    bevelSize={0.02}
-                    bevelThickness={0.1}
-                    height={0.5}
-                    scale={[1, 1, 0.25]}
-                    size={0.4}
-                    lineHeight={0.5}>
-                    {`Larry's Portfolio`}
-                    <meshNormalMaterial />
-                </drei.Text3D>
-                <drei.Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
-                    <meshStandardMaterial attach="material" color="lightblue" />
-                </drei.Plane>
+            <drei.KeyboardControls
+                map={[
+                    { name: "forward", keys: ["ArrowUp", "w", "W"] },
+                    { name: "backward", keys: ["ArrowDown", "s", "S"] },
+                    { name: "left", keys: ["ArrowLeft", "a", "A"] },
+                    { name: "right", keys: ["ArrowRight", "d", "D"] },
+                ]}>
+                <Canvas>
+                    <drei.Stars radius={500} depth={100} count={1000} factor={5} />
+                    <fog attach="fog" args={['#202025', 0, 80]} />
+                    <Frame position={[10, 0, 0.5]} />
+                    <FrameVideo url="/10.mp4" position={[-3, 0, 1]} rotation={[0, 0.9, 0]} />
+                    <drei.Text3D
+                        position={[-2, 1, -1.5]}
+                        font="/Inter_Bold.json"
+                        curveSegments={32}
+                        bevelEnabled
+                        bevelSize={0.02}
+                        bevelThickness={0.1}
+                        height={0.5}
+                        scale={[1, 1, 0.25]}
+                        size={0.4}
+                        lineHeight={0.5}>
+                        {`Larry's Portfolio`}
+                        <meshNormalMaterial />
+                    </drei.Text3D>
 
-                <ambientLight intensity={0.25} />
-                <pointLight position={[0, 5, 10]} intensity={0.5} />
+                    <drei.Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
+                        <meshStandardMaterial attach="material" color="lightblue" />
+                    </drei.Plane>
 
-                <XR>
-                    <SmoothLocomotion />
-                    <SnapRotation />
-                    <Controllers />
-                    <Hands />
-                </XR>
-            </Canvas >
+                    <ambientLight intensity={0.25} />
+                    <pointLight castShadow position={[0, 5, 10]} intensity={0.5} />
+
+                    <XR>
+                        <SmoothLocomotion />
+                        <SnapRotation />
+                        <Controllers />
+                        <Hands />
+                    </XR>
+                </Canvas >
+            </drei.KeyboardControls>
             <drei.Loader />
         </>
     )

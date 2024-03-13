fb16d86a77751c7e727ce2f92737a6534469d076
Larry Pozdeev <larrypozdeev@gmail.com>
2024-03-13 01:28:57 -0700

    implements video player, adds text

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,88 @@
 import { Hands, VRButton, XR, Controllers } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
-import { Plane } from '@react-three/drei';
+import * as drei from '@react-three/drei';
 import SmoothLocomotion from './SmoothLocomotion';
 import SnapRotation from './SnapRotation';
-import { useLocation } from 'wouter';
-import * as THREE from 'three';
-import getUuid from 'uuid-by-string';
+import { Suspense, useMemo } from 'react';
+import { Vector3 } from '@dimforge/rapier3d-compat';
+
+
 
 const GOLDENRATIO = 1.61803398875;
 
-function Frames() {
-    const [, setLocation] = useLocation();
-    const items = [1, 2, 3, 4, 5].map((idx) => ({
-        idx
-    }));
-    return (
-        <group
-            onPointerMissed={() => setLocation('/')}>
-            {items.map((props) => <Frame key={props.idx} {...props} /> /* prettier-ignore */)}
-        </group>
-    )
-}
 
-function Frame({ idx, c = new THREE.Color(), ...props }: { idx: number; c?: THREE.Color }) {
-    const name = getUuid(idx.toString());
-
+function Frame({ ...props }) {
+    const scale = props.scale ? props.scale : [1, GOLDENRATIO, 0.05];
     return (
-        <group {...props}>
-            <mesh
-                name={name}
-                scale={[1, GOLDENRATIO, 0.05]}
-                position={[0, GOLDENRATIO / 2, 0]}>
+        <mesh
+            {...props}
+            scale={scale}>
+            <boxGeometry />
+
+            <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+            <mesh scale={[0.93, 0.93, 0.9]} position={[0, 0, 0.2]}>
                 <boxGeometry />
-                <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-                <mesh raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
-                    <boxGeometry />
-                    <meshBasicMaterial toneMapped={false} fog={false} />
-                </mesh>
+                <meshBasicMaterial toneMapped={false} fog={false} />
             </mesh>
-        </group>
+        </mesh>
     )
 }
 
+function FrameVideo({ url, ...props }: { url: string, rotation?: any, position?: [number, number, number], scale?: [number, number, number] }) {
+    const texture = drei.useVideoTexture(url);
+    const videoPos: [number, number, number] | undefined = props.position
+        ? [props.position[0] + Math.sin(props.rotation[1] * Math.PI / 4) * 0.05, props.position[1], props.position[2] + Math.cos(props.rotation[1] * Math.PI / 4) * 0.05]
+        : undefined;
+    const scale = props.scale ? props.scale : [GOLDENRATIO, 1, 0.05] as [number, number, number];
+    return (
+        <>
+            <Frame scale={scale} {...props}>
+            </Frame>
+            <mesh {...props} position={videoPos}>
+                <planeGeometry args={[scale[0] * 0.95, scale[1] * 0.95]} />
+                <meshBasicMaterial map={texture} />
+            </mesh>
+        </>
+    )
+}
 function App() {
     return (
         <>
             <VRButton />
             <Canvas>
-                <XR >
-                    <Controllers />
-                    <Hands />
-                    <ambientLight intensity={0.9} />
-                    <pointLight position={[10, 10, 10]} />
-                    <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-                        <meshStandardMaterial attach="material" color="lightblue" />
-                    </Plane>
-                    <group position={[0, -0.5, 0]}>
-                        <Frames />
-                    </group>
+                <drei.Stars radius={500} depth={100} count={1000} factor={5} />
+                <fog attach="fog" args={['#202025', 0, 80]} />
+                <Frame position={[10, 0, 0.5]} />
+                <FrameVideo url="/10.mp4" position={[-3, 0, 1]} rotation={[0, 0.9, 0]} />
+                <drei.Text3D
+                    position={[-2, 1, -1.5]}
+                    font="/Inter_Bold.json"
+                    curveSegments={32}
+                    bevelEnabled
+                    bevelSize={0.02}
+                    bevelThickness={0.1}
+                    height={0.5}
+                    scale={[1, 1, 0.25]}
+                    size={0.4}
+                    lineHeight={0.5}>
+                    {`Larry's Portfolio`}
+                    <meshNormalMaterial />
+                </drei.Text3D>
+                <drei.Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
+                    <meshStandardMaterial attach="material" color="lightblue" />
+                </drei.Plane>
+
+                <ambientLight intensity={0.25} />
+                <pointLight position={[0, 5, 10]} intensity={0.5} />
+
+                <XR>
                     <SmoothLocomotion />
                     <SnapRotation />
+                    <Controllers />
+                    <Hands />
                 </XR>
-            </Canvas>
+            </Canvas >
+            <drei.Loader />
         </>
     )
 

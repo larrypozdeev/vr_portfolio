768891771cb202c8397958074b0d531f31d7694b
Larry Pozdeev <larrypozdeev@gmail.com>
2024-03-20 00:33:26 -0700

    adds moon surface, adds collisions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
 import { Hands, VRButton, XR, Controllers } from '@react-three/xr'
+
 import { Canvas } from '@react-three/fiber'
 import * as drei from '@react-three/drei';
 import SmoothLocomotion from './SmoothLocomotion';
 import SnapRotation from './SnapRotation';
+import { FrameVideo } from './Frame';
+import { Physics, RigidBody, CuboidCollider } from '@react-three/rapier'
+
 import { Suspense } from 'react';
-import { Frame, FrameImage, FrameVideo } from './Frame';
 import React from 'react';
-import { Text3D } from '@react-three/drei';
 
 function DefaultScene(props: any) {
     let turnedRotation = [props.rotation[0], props.rotation[1] + Math.PI / 6, props.rotation[2]] as [number, number, number];
     let textPosition = [props.position[0], -0.25, props.position[2]] as [number, number, number];
+
     return (
         <>
             <drei.Center right position={textPosition} rotation={props.rotation}>
-                <drei.Text3D
-                    position={props.position}
-                    font="/Inter_Bold.json"
-                    curveSegments={32}
-                    bevelEnabled
-                    bevelSize={0.02}
-                    bevelThickness={0.1}
-                    height={0.5}
-                    scale={[1, 1, 0.25]}
-                    size={0.4}
-                    lineHeight={0.5}
-                    castShadow>
-                    {props.name}
-                    <meshStandardMaterial color={"#eb7734"} />
-                </drei.Text3D>
+                <RigidBody>
+                    <drei.Text3D
+                        position={[0, 0, 0]}
+                        font="/Inter_Bold.json"
+                        curveSegments={32}
+                        bevelEnabled
+                        bevelSize={0.02}
+                        bevelThickness={0.1}
+                        height={0.5}
+                        scale={[1, 1, 0.25]}
+                        size={0.4}
+                        lineHeight={0.5}
+                        castShadow>
+                        {props.name}
+                        <meshStandardMaterial color={"#eb7734"} />
+                    </drei.Text3D>
+                </RigidBody>
             </drei.Center>
             <Suspense fallback={"10.mp4"}>
                 <FrameVideo url={props.url} position={props.position} rotation={turnedRotation} />
@@ -68,31 +73,63 @@ function LowLevelLanguageScene(props: any) {
     )
 }
 
+function MoonSurface(props: any) {
+    const { nodes, materials } = drei.useGLTF('/scene.glb') as any;
+
+    return (
+        <group {...props} dispose={null}>
+            <group rotation={[-Math.PI / 2, 0, 0]} scale={60}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Petavius_ZB_LP_1_0.geometry}
+                    material={materials.material}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Petavius_ZB_LP_2_0.geometry}
+                    material={materials.material_1}
+                />
+            </group>
+        </group>
+    )
+}
+drei.useGLTF.preload('/scene.glb');
+
 function MainScene({ children }: { children: React.ReactNode }) {
     let i = 0;
     let radius = 20;
     return (
         <>
-            <drei.Text3D
-                position={[-2, 1, -1.5]}
-                font="/Inter_Bold.json"
-                curveSegments={32}
-                bevelEnabled
-                bevelSize={0.02}
-                bevelThickness={0.1}
-                height={0.5}
-                scale={[1, 1, 0.25]}
-                size={0.4}
-                lineHeight={0.5}
-                castShadow>
-                {`Larry's Portfolio`}
-                <meshStandardMaterial color={"#eb7734"} />
-            </drei.Text3D>
+            <RigidBody name='world' colliders="trimesh" position={[0, 0, 0]}>
+                <MoonSurface position={[0, 0, 0]} />
+            </RigidBody>
+
+            {'Portfolio'.split('').map((letter, index) => {
+                return (
+                    <RigidBody key={index} colliders='cuboid' position={[index * 0.5, 3, 0]}>
+                        <drei.Text3D
+                            position={[0, 0, 0]}
+                            font="/Inter_Bold.json"
+                            curveSegments={32}
+                            bevelEnabled
+                            bevelSize={0.02}
+                            bevelThickness={0.1}
+                            height={0.5}
+                            scale={[1, 1, 0.25]}
+                            size={0.4}
+                            lineHeight={0.5}
+                            castShadow>
+                            {letter}
+                            <meshStandardMaterial color={"#eb7734"} />
+                        </drei.Text3D>
+                    </RigidBody>
+                )
+                })}
 
             {React.Children.map(children, (child) => {
                 if (React.isValidElement(child)) {
-                    // position in a circle
-                    // there's no children props to be accessed
                     let position = [
                         Math.sin(i * Math.PI / 2) * radius,
                         1,
@@ -107,6 +144,7 @@ function MainScene({ children }: { children: React.ReactNode }) {
                     })
                 }
             })}
+
         </>
     )
 }
@@ -115,17 +153,11 @@ function EnvSettings() {
         <>
             <drei.OrbitControls />
             <drei.Stars radius={200} depth={100} count={1000} factor={5} />
-            <fog attach="fog" args={['#202025', 0, 80]} />
-
-            <drei.Plane receiveShadow args={[200, 200]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
-                <meshStandardMaterial attach="material" color="lightblue" />
-            </drei.Plane>
-
+            <fog attach="fog" args={['#202025', 0, 60]} />
             <drei.SoftShadows />
-            <drei.BakeShadows />
             <drei.ShadowAlpha />
             <drei.Environment preset="night" shadow-mapSize={2048} />
-            <directionalLight position={[0, 10, 10]} intensity={0.5} castShadow shadow-mapSize={2048} />
+            <directionalLight position={[0, 10, 10]} intensity={1.5} castShadow shadow-mapSize={2048} />
         </>
     );
 }
@@ -141,23 +173,40 @@ function XRSettings() {
         </>
     )
 }
+function Loader() {
+    const { progress } = drei.useProgress();
+    return (
+        <drei.Html center>
+            <h1 style={{ color: 'white' }}>
+                {Math.round(progress)}%&nbsp;loaded
+            </h1>
+        </drei.Html>
+    )
+}
+
 function App() {
     return (
         <>
             <VRButton />
-            <Canvas shadows camera={{ position: [0, 3, 0] }}>
-                <EnvSettings />
 
-                <MainScene>
-                    <WebScene />
-                    <GameDevScene />
-                    <DataWorkScene />
-                    <LowLevelLanguageScene />
-                </MainScene>
+            <Canvas shadows camera={{ position: [0, 5, 0] }}>
+                <Suspense fallback={<Loader />}>
+                    <Physics>
+                        <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
+                        <EnvSettings />
 
-                <XRSettings />
+                        <MainScene>
+                            <WebScene />
+                            <GameDevScene />
+                            <DataWorkScene />
+                            <LowLevelLanguageScene />
+                        </MainScene>
+
+                        <XRSettings />
+                    </Physics>
+                </Suspense>
+                <drei.Preload all />
             </Canvas >
-            <drei.Loader />
         </>
     )
 
